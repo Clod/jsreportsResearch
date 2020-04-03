408d333f3fab5a447085513f1594e7ba8776b6ba
ClaudioGrasso <claudio.grasso@yahoo.com>
2020-04-03 19:37:19 -0300

    Primera versión que logra armar un reporte que combina datos de Firebase y Zendrive Arma una planilla con un listado de conductores y sus promedios de puntaje y trae nombre y apellido de Firebase

--- primero-script/content.js
+++ primero-script/content.js
@@ -5,6 +5,7 @@ const admin = require("firebase-admin");
 // curl https://api.zendrive.com/v3/drivers?apikey=Mf8ZOERWLnwUuRLKLw4fnUhOsJChMbW9
 
 
+
 async function beforeRender(req, res) {
     console.log("***********   En beforeRender **********************")
 
@@ -18,16 +19,16 @@ async function beforeRender(req, res) {
 
     do {
 
+        // Leo de Zendrive
         var url = 'https://api.zendrive.com/v3/drivers?apikey=Mf8ZOERWLnwUuRLKLw4fnUhOsJChMbW9&start_date=2019-09-01&end_date=2020-03-23&offset=' + next_offset
 
         console.log(url)
 
+        // Leo de Zendrive
         var r = await axios.get(url)
 
         //var r = await axios.get('https://api.zendrive.com/v3/drivers?apikey=Mf8ZOERWLnwUuRLKLw4fnUhOsJChMbW9&start_date=2019-09-01&end_date=2020-03-23&offset=0')
         // req.data = { ...req.data, ...r.data}
-
-
 
         req.data.zenResp = r.data
 
@@ -36,12 +37,13 @@ async function beforeRender(req, res) {
         console.log("drivers= " + drivers)
 
         if (drivers == '') {
-            console.log("ARRIBA " + r.data.drivers[1].driver_id)
+            console.log("Primera lectura: " + r.data.drivers[1].driver_id)
+            // Transfiero lo leído a un array local
             drivers = r.data.drivers
             console.log("TIPO de dato: " + typeof r.data.drivers);
         } else {
             console.log("CONCATENANDO " + r.data.drivers[1].driver_id)
-            // drivers.concat(r.data.drivers)
+            // Concateno los nuevos drivers que traje de Zendrive a los ya leídos.
             Array.prototype.push.apply(drivers, r.data.drivers);
         }
 
@@ -54,14 +56,14 @@ async function beforeRender(req, res) {
         console.log("NEXT OFFSET = " + r.data.next_offset)
 
     }
-    while (next_offset)
+    while (next_offset > 0) // Voy a buscar la página siguiente si la hay (next_offset > 0)
 
+    // logueo por consola lo leído de Zendrive
     console.log("TOTAL: " + JSON.stringify(drivers))
-
-    req.data.drivers = drivers
 
     // Ahora voy a intentar traer desde Firebase el nombre y apellido de cada uno
 
+    // Inicializo la conexión a Firestore si es necesario
     if (admin.apps.length === 0) {
 
         console.log("Inicializando base");
@@ -84,16 +86,21 @@ async function beforeRender(req, res) {
 
     db = admin.firestore();
 
-    let driverRef = db.collection('boards').doc('ARTTTAAAFABA');
+    ///////////////////////////////////////////////////////////////////////
 
-    let tmp = ""
+    console.log("Buscando datos de uno: " + drivers[8].driver_id);
+
+    let driverRef = db.collection('boards').doc(drivers[8].driver_id);
+
+    console.log("driverRef: " + JSON.stringify(driverRef));
 
     let getDoc = await driverRef.get()
         .then(doc => {
+            console.log('En el then');
             if (!doc.exists) {
                 console.log('No such document!');
             } else {
-                console.log('Document data:', doc.data().ConductorID);
+                console.log('Document data:', doc.data().ConductorID + "  " + doc.data().Nombre + "  " + doc.data().Apellido);
                 tmp = doc.data().ConductorID;
             }
         })
@@ -101,11 +108,44 @@ async function beforeRender(req, res) {
             console.log('Error getting document', err);
         });
 
+    ///////////////////////////////////////////////////////////////////////
+
+
+    for (i = 0; i < drivers.length; i++) {
+
+        console.log("Buscando datos: " + drivers[i].driver_id);
+
+        let driverRef = db.collection('boards').doc(drivers[i].driver_id);
+
+        // console.log("driverRef: " + JSON.stringify(driverRef));
+
+        let getDoc = await driverRef.get()
+            .then(doc => {
+                console.log('En el then');
+                if (!doc.exists) {
+                    console.log('No such document!');
+                    drivers[i].info.attributes.last_name = 'Cadorna';
+                    drivers[i].info.attributes.first_name = 'Luigi';
+                } else {
+                    console.log('Document data:', doc.data().ConductorID + "  " + doc.data().Nombre + "  " + doc.data().Apellido);
+                    drivers[i].info.attributes.last_name = doc.data().Apellido;
+                    drivers[i].info.attributes.first_name = doc.data().Nombre;
+                    //tmp = doc.data().ConductorID;
+                }
+            })
+            .catch(err => {
+                console.log('Error getting document', err);
+            });
+
+    };
+
+    req.data.drivers = drivers
+
     req.data.unDriver = ' algo'
-    req.data.check = tmp
+    //req.data.check = tmp
 
     console.log("Llegué al final")
 
-    console.log(tmp)
+    // console.log(tmp)
 
 }
